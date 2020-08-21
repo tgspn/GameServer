aafc362b1e349a2b8a09547c1e86f418186ee38f
Tiago <tiago.spana@gmail.com>
2020-08-21 00:10:32 -0300

    doc(swagger):documentation for swagger

--- GameServer/Controllers/GameController.cs
+++ GameServer/Controllers/GameController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,17 +22,23 @@ namespace GameServer.Controllers
             this.service = new GameService();
             this.logger = logger;
         }
-
+        /// <summary>
+        /// Save the game result
+        /// </summary>
+        /// <param name="model">game result to save</param>
+        /// <returns></returns>
+        /// <response code="200">if success save</response>
+        /// <response code="400">If payload body is not valid</response>
+        /// <response code="500">If any error occur</response>
         [HttpPost]
-        public async Task<IActionResult> GameResult(GameResultViewModel model)
+        public IActionResult GameResult(GameResultViewModel model)
         {
             try
             {
-
                 if (!ModelState.IsValid)
                     return StatusCode(400, "the result is not valid");
-                await Task.Run(() => { service.Save((GameResultModel)model); });
-                
+                service.Save((GameResultModel)model);
+
                 return Ok();
             }
             catch (Exception ex)
--- GameServer/Controllers/LeaderboardController.cs
+++ GameServer/Controllers/LeaderboardController.cs
@@ -22,15 +22,22 @@ namespace GameServer.Controllers
             this.logger = logger;
             this.service = LeaderboardService.Instance;
         }
-
+        /// <summary>
+        /// return the leaderboard from the game
+        /// </summary>
+        /// <param name="gameId">Game Id to get leaderboard</param>
+        /// <returns>A Top 100 leaderboard</returns>
+        /// <response code="200">Returns the top 100 leaderboard</response>
+        /// <response code="404">If game Id not found</response>
+        /// <response code="500">If any error occur</response>
         [HttpGet]
-        [Route("{gameId}")]
-        public  ActionResult<IEnumerable<LeaderboardViewModel>> LeaderBoard(long gameId)
+        [Route("{gameId}")]        
+        public  ActionResult<IEnumerable<LeaderboardViewModel>> Leaderboard(long gameId)
         {
             try
             {
                 if (!service.ContainsGame(gameId))
-                    return StatusCode(404, "No game found");
+                    return StatusCode(404, "Game not found");
 
                 return StatusCode(200, service[gameId]);
             }
