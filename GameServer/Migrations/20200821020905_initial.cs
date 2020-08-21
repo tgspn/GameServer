6dbe4d1fe4cff82d94be2740e10211a365717281
Tiago <tiago.spana@gmail.com>
2020-08-21 00:03:40 -0300

    feat(migration) :add migration

---
+++ GameServer/Migrations/20200821020905_initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameServer.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameResult",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    PlayerId = table.Column<long>(nullable: false),
+                    GameId = table.Column<long>(nullable: false),
+                    Win = table.Column<long>(nullable: false),
+                    Timestamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameResult", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameResult");
+        }
+    }
+}
